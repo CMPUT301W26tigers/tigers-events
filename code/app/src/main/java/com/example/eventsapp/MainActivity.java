8e6e7ecff741d9f130222a996c174daa895a2b21
BrntChip <127444260+BrntChip@users.noreply.github.com>
2026-03-10 17:24:10 -0600

    fixes

--- code/app/src/main/java/com/example/eventsapp/MainActivity.java
+++ code/app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -23,7 +22,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements EventDialogFragment.EventDialogListener, DeleteEventDialogFragment.OnFragmentInteractionListener {
 
@@ -40,114 +38,112 @@ public class MainActivity extends AppCompatActivity implements EventDialogFragme
     private CollectionReference eventsRef;
 
 
-    public class MainActivity extends AppCompatActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(res.layout.activity_main);
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-            // Set views
-            addEventButton = findViewById(R.id.buttonAddEvent);
-            deleteEventButton = findViewById(R.id.buttonDeleteEvent);
-            eventListView = findViewById(R.id.listviewCities);
+        // Set views
+        addEventButton = findViewById(R.id.buttonAddEvent);
+        deleteEventButton = findViewById(R.id.buttonDeleteEvent);
+        eventListView = findViewById(R.id.listviewCities);
 
 
-            // create event array
-            eventArrayList = new ArrayList<>();
-            eventArrayAdapter = new EventArrayAdapter(this, eventArrayList);
-            eventListView.setAdapter(eventArrayAdapter);
+        // create event array
+        eventArrayList = new ArrayList<>();
+        eventArrayAdapter = new EventArrayAdapter(this, eventArrayList);
+        eventListView.setAdapter(eventArrayAdapter);
 
-            // set listeners
-            addEventButton.setOnClickListener(view -> {
-                EventDialogFragment eventDialogFragment = new EventDialogFragment();
-                eventDialogFragment.show(getSupportFragmentManager(), "Add Event");
-            });
+        // set listeners
+        addEventButton.setOnClickListener(view -> {
+            EventDialogFragment eventDialogFragment = new EventDialogFragment();
+            eventDialogFragment.show(getSupportFragmentManager(), "Add Event");
+        });
 
-            deleteEventButton.setOnClickListener(view -> {
-                DeleteEventDialogFragment deleteEventDialogFragment = new DeleteEventDialogFragment();
-                deleteEventDialogFragment.show(getSupportFragmentManager(), "Delete Event");
-            });
+        deleteEventButton.setOnClickListener(view -> {
+            DeleteEventDialogFragment deleteEventDialogFragment = new DeleteEventDialogFragment();
+            deleteEventDialogFragment.show(getSupportFragmentManager(), "Delete Event");
+        });
 
-            eventListView.setOnItemClickListener((adapterView, view, i, l) -> {
-                Event event = eventArrayAdapter.getItem(i);
-                EventDialogFragment eventDialogFragment = EventDialogFragment.newInstance(event);
-                eventDialogFragment.show(getSupportFragmentManager(), "Event Details");
-            });
+        eventListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Event event = eventArrayAdapter.getItem(i);
+            EventDialogFragment eventDialogFragment = EventDialogFragment.newInstance(event);
+            eventDialogFragment.show(getSupportFragmentManager(), "Event Details");
+        });
 
-            db = FirebaseFirestore.getInstance();
-            eventsRef = db.collection("events");
+        db = FirebaseFirestore.getInstance();
+        eventsRef = db.collection("events");
 
-            eventsRef.addSnapshotListener((value, error) -> {
-                if (error != null) {
-                    Log.e("Firestore", error.toString());
-                    return;
-                }
-                if (value != null) {
-                    eventArrayList.clear();
-                    for (QueryDocumentSnapshot snapshot : value) {
-                        String name = snapshot.getString("name");
-                        Long amountLong = snapshot.getLong("amount");
-                        int amount = (amountLong != null) ? amountLong.intValue() : 0;
+        eventsRef.addSnapshotListener((value, error) -> {
+            if (error != null) {
+                Log.e("Firestore", error.toString());
+                return;
+            }
+            if (value != null) {
+                eventArrayList.clear();
+                for (QueryDocumentSnapshot snapshot : value) {
+                    String name = snapshot.getString("name");
+                    Long amountLong = snapshot.getLong("amount");
+                    int amount = (amountLong != null) ? amountLong.intValue() : 0;
 
-                        if (amount != 0) {
-                            eventArrayList.add(new Event(name, amount));
-                        }
+                    if (amount != 0) {
+                        eventArrayList.add(new Event(name, amount));
                     }
-                    eventArrayAdapter.notifyDataSetChanged();
                 }
-            });
-        }
+                eventArrayAdapter.notifyDataSetChanged();
+            }
+        });
+    }
 
-        @Override
-        public void updateEvent(Event event, String title, int amount) {
-            event.setName(title);
-            event.setAmount(amount);
-            eventArrayAdapter.notifyDataSetChanged();
+    @Override
+    public void updateEvent(Event event, String title, int amount) {
+        event.setName(title);
+        event.setAmount(amount);
+        eventArrayAdapter.notifyDataSetChanged();
 
-            DocumentReference docRef = eventsRef.document(event.getName());
-            docRef.update("name", title,
-                            "amount", amount)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d("Firestore", "DocumentSnapshot successfully written!");
-                        }
-                    });
-            // Updating the database using delete + addition
-        }
+        DocumentReference docRef = eventsRef.document(event.getName());
+        docRef.update("name", title,
+                        "amount", amount)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Firestore", "DocumentSnapshot successfully written!");
+                    }
+                });
+        // Updating the database using delete + addition
+    }
 
-        @Override
-        public void addEvent(Event event) {
-            eventArrayList.add(event);
-            eventArrayAdapter.notifyDataSetChanged();
+    @Override
+    public void addEvent(Event event) {
+        eventArrayList.add(event);
+        eventArrayAdapter.notifyDataSetChanged();
 
-            DocumentReference docRef = eventsRef.document(event.getName());
-            docRef.set(event)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d("Firestore", "DocumentSnapshot successfully written!");
-                        }
-                    });
+        DocumentReference docRef = eventsRef.document(event.getName());
+        docRef.set(event)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Firestore", "DocumentSnapshot successfully written!");
+                    }
+                });
 
-        }
+    }
 
-        @Override
-        public void onConfirmPressed(String eventName) {
-            eventsRef.document(eventName).delete();
+    @Override
+    public void onConfirmPressed(String eventName) {
+        eventsRef.document(eventName).delete();
 
-            BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
 
-            NavHostFragment navHost =
-                    (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavHostFragment navHost =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
 
-            NavController navController = navHost.getNavController();
-            NavigationUI.setupWithNavController(bottomNav, navController);
-        }
+        NavController navController = navHost.getNavController();
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
