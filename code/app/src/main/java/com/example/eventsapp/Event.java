f2e08427630a8adc5579fd30cb27543c601618b7
Dequawn <dequawn@ualberta.ca>
2026-04-02 12:28:46 -0600

    Added waitlist parameter and changed edit event xml to decouple waitlist capacity from event capacity

--- code/app/src/main/java/com/example/eventsapp/Event.java
+++ code/app/src/main/java/com/example/eventsapp/Event.java
@@ -21,6 +21,7 @@ public class Event implements Serializable {
     private String description;
     private String posterUrl;
     private int sampleSize;  // US 02.05.02: number of attendees to sample/invite
+    private int waitlistCapacity; // 0 represents unlimited. US 02.03.01 (added by O'Mario)
     private String hostId;   // ID of the user who created this event
     private boolean geolocationRequired;
     private transient String entrantStatus; //used for UI display only
@@ -197,6 +198,23 @@ public class Event implements Serializable {
     public void setSampleSize(int sampleSize) {
         this.sampleSize = Math.max(0, sampleSize);
     }
+
+    /**
+     * Gets the waitlist capacity.
+     * @return The waitlist capacity (0 = unlimited).
+     */
+    public int getWaitlistCapacity() {
+        return waitlistCapacity;
+    }
+
+    /**
+    * Sets the waitlist capacity limit.
+    * @param waitlistCapacity The capacity to set.
+    */
+    public void setWaitlistCapacity(int waitlistCapacity) {
+        this.waitlistCapacity = Math.max(0, waitlistCapacity);
+    }
+
 
     /**
      * Gets the ID of the user who created this event.
